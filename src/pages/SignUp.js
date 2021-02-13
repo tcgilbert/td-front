d9ba952a933fca9f2b8ae9e1ffb826cc8e38eea9
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-13 14:47:03 -0500

    [Signup] basic form setup and styling

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,23 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const useStyles = makeStyles((theme) => ({
     textField: {
         width: '90%',
-        marginTop: '1rem,'
+        marginTop: '2rem',
+    },
+    input: {
+        fontSize: '2rem',
+        fontWeight: '300'
+    },
+    label: {
+        fontSize: '1.5rem'
+    },
+    adornedStart: {
+        fontSize: '1.8rem',
+        color: 'rgb(150, 150, 150)'
     }
 }))
 
@@ -17,10 +29,54 @@ const SignUp = () => {
             <div className="signup__form">
                 <TextField
                     className={classes.textField}
-                    label="Name"
+                    InputProps={{
+                        className: classes.input,
+                        startAdornment: <InputAdornment position="start"><span className={classes.adornedStart}>thesedays.io/</span></InputAdornment>
+                    }}
+                    InputLabelProps={{
+                        className: classes.label
+                    }}
+                    label="Username"
                     type="text"
-                    name="name"
+                    name="username"
                 />
+                <TextField
+                    className={classes.textField}
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        className: classes.label
+                    }}
+                    label="Email"
+                    type="text"
+                    name="email"
+                />
+                <TextField
+                    className={classes.textField}
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        className: classes.label
+                    }}
+                    label="Password"
+                    type="text"
+                    name="password"
+                />
+                <TextField
+                    className={classes.textField}
+                    InputProps={{
+                        className: classes.input
+                    }}
+                    InputLabelProps={{
+                        className: classes.label
+                    }}
+                    label="Confirm Password"
+                    type="text"
+                    name="confirmPassword"
+                />
+                <button className="signup__btn">Create Account</button>
             </div>
         </div>
     );
