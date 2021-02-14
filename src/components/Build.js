4fc12c93832567eaa7ea60c7a27fa01139387201
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-14 13:46:43 -0500

    [Build] hiding and showing component drawer

--- src/components/Build.js
+++ src/components/Build.js
@@ -1,31 +1,38 @@
 import React, { useEffect } from "react";
-import Arrow from "../images/arrow.svg"
+import Arrow from "../images/arrow.svg";
 
 const Build = () => {
-
-
     useEffect(() => {
-        const check = document.getElementById("show")
-        const container = document.getElementById("build-container")
+        const check = document.getElementById("show");
+        const container = document.getElementById("build-container");
 
         container.addEventListener("click", () => {
             if (check.getAttribute("checked")) {
-                check.removeAttribute("checked")
+                check.removeAttribute("checked");
             } else {
-                check.setAttribute("checked", true)
+                check.setAttribute("checked", true);
             }
-        })
-    }, [])
-
-
+        });
+    }, []);
 
     return (
         <div className="build">
+            <input
+                className="build__show"
+                type="checkbox"
+                name="show"
+                id="show"
+            />
             <div className="build__components" id="build-container">
                 <p className="build__text">Components</p>
-                <input className="build__show" type="checkbox" name="show" id="show"/>
                 <img className="build__arrow" src={Arrow} alt="Down Arrow" />
-                <div className="build__pillbox"></div>
+            </div>
+            <div className="build__pillbox">
+                <p className="build__pill">Text</p>
+                <p className="build__pill">Soundtrack</p>
+                <p className="build__pill">Book</p>
+                <p className="build__pill">Podcast</p>
+                <p className="build__pill">Movie</p>
             </div>
         </div>
     );
