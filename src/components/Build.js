a056ec2934c11f73fe94cacdc87f8d1cc37711ea
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-14 18:39:20 -0500

    [Build] selectors for build option functioning

--- src/components/Build.js
+++ src/components/Build.js
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
 import Arrow from "../images/arrow.svg";
-import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const Build = () => {
+    const [buildOption, setBuildOption] = useState("text");
+
+
+    // For build options
+    useEffect(() => {
+        const text = document.getElementById("text");
+        const sound = document.getElementById("sound");
+        const book = document.getElementById("book");
+        const pod = document.getElementById("pod");
+        const movie = document.getElementById("movie");
+        const link = document.getElementById("link");
+
+        const buildOptions = [text, sound, book, pod, movie, link];
+        buildOptions.forEach((opt) => {
+            if (opt.id === buildOption) {
+                opt.classList.add("build__selected");
+            } else {
+                if (opt.classList.contains("build__selected")) {
+                    opt.classList.remove("build__selected");
+                }
+            }
+        });
+    }, [buildOption]);
+
+    // For checkbox
     useEffect(() => {
         const check = document.getElementById("show");
         const container = document.getElementById("build-container");
@@ -30,17 +55,28 @@ const Build = () => {
             </div>
             <div className="build__options">
                 <div className="build__pillbox">
-                    <p className="build__pill">Text</p>
-                    <p className="build__pill">Soundtrack</p>
-                    <p className="build__pill">Book</p>
-                    <p className="build__pill">Podcast</p>
-                    <p className="build__pill">Movie</p>
-                    <p className="build__pill">Link</p>
+                    <p onClick={() => setBuildOption("text")} id="text" className="build__pill">
+                        Text
+                    </p>
+                    <p onClick={() => setBuildOption("sound")} id="sound" className="build__pill">
+                        Soundtrack
+                    </p>
+                    <p onClick={() => setBuildOption("book")} id="book" className="build__pill">
+                        Book
+                    </p>
+                    <p onClick={() => setBuildOption("pod")} id="pod" className="build__pill">
+                        Podcast
+                    </p>
+                    <p onClick={() => setBuildOption("movie")} id="movie" className="build__pill">
+                        Movie
+                    </p>
+                    <p onClick={() => setBuildOption("link")} id="link" className="build__pill">
+                        Link
+                    </p>
                 </div>
                 <div className="build__form-container">
                     <div className="build__form">
-                        <input type="text"/>
-                        <input type="text"/>
+                        <input type="text" />
                     </div>
                 </div>
             </div>
