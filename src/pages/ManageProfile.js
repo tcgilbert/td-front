7df0d82fe8c8c34c6d5dafe22d3e2ee29884a1ff
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-14 12:52:34 -0500

    [ManageProfile] added arrow

--- src/pages/ManageProfile.js
+++ src/pages/ManageProfile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PrivateNav from "../components/PrivateNav";
+import Arrow from "../images/arrow.svg"
 
 const ManageProfile = (props) => {
     const [location, setLocation] = useState("build");
@@ -60,7 +61,10 @@ const ManageProfile = (props) => {
             <div className="grid manage__grid3">
                 <div className="build">
                     <div className="build__components">
+                        <input type="checkbox" name="show" id="show"/>
                         <p className="build__text">Components</p>
+                        <img className="build__arrow" src={Arrow} alt="Down Arrow"/>
+                        <div className="build__pillbox"></div>
                     </div>
                 </div>
             </div>
