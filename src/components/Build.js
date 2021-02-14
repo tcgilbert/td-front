9597313a1fa987ae7a0e2eea1d841fe6206b34a3
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-14 13:19:57 -0500

    [Build] successfully flipping arrow

--- src/components/Build.js
+++ src/components/Build.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Arrow from "../images/arrow.svg"
 
 const Build = () => {
+
+
+    useEffect(() => {
+        const check = document.getElementById("show")
+        const container = document.getElementById("build-container")
+
+        container.addEventListener("click", () => {
+            if (check.getAttribute("checked")) {
+                check.removeAttribute("checked")
+            } else {
+                check.setAttribute("checked", true)
+            }
+        })
+    }, [])
+
+
+
     return (
         <div className="build">
-            <div className="build__components">
-                <input type="checkbox" name="show" id="show" />
+            <div className="build__components" id="build-container">
                 <p className="build__text">Components</p>
+                <input className="build__show" type="checkbox" name="show" id="show"/>
                 <img className="build__arrow" src={Arrow} alt="Down Arrow" />
                 <div className="build__pillbox"></div>
             </div>
