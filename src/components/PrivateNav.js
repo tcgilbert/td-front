a5a7d4e93681b7d941bac48e1edd4b71467126a0
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-15 16:39:59 -0500

    [Manage] properly fetching about content

--- src/components/PrivateNav.js
+++ src/components/PrivateNav.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 const PrivateNav = (props) => {
-    console.log(props.user);
     return (
         <div className="private-nav">
             <svg
