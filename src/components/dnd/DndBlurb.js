b169f3a6d44eca8467597ba62bc1a4329c3b22f5
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-18 10:28:43 -0500

    [DndBlurb] edit form functionality

--- src/components/dnd/DndBlurb.js
+++ src/components/dnd/DndBlurb.js
@@ -2,14 +2,100 @@ import React, { useState } from "react";
 import Trash from "../../images/trash.svg";
 import Edit from "../../images/edit.svg";
 import DeleteDiv from "./DeleteDiv";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextareaAutosize } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    blurb: {
+        display: "flex",
+        flexDirection: "column",
+        padding: ".5rem",
+    },
+    heading: {
+        fontSize: "1.8rem",
+        fontWeight: "500",
+    },
+    content: {
+        marginTop: ".5rem",
+        resize: "vertical",
+    },
+    input: {
+        fontSize: "2rem",
+        fontWeight: "400",
+    },
+}));
 
 const DndBlurb = (props) => {
+    const classes = useStyles();
     const [deleteSelected, setDeleteSelected] = useState(false);
+    const [editSelected, setEditSelected] = useState(false);
+    const [heading, setHeading] = useState(props.ele.content.heading);
+    const [content, setContent] = useState(props.ele.content.content);
+
     const handleDisplay = () => {
         if (deleteSelected) {
             setDeleteSelected(false);
         } else {
             setDeleteSelected(true);
+        }
+    };
+
+    const contentChange = (e) => {
+        const save = document.getElementById("save-btn");
+        setContent(e.target.value);
+        if (save.classList.contains("blurb__nochange")) {
+            save.classList.remove("blurb__nochange");
+            save.classList.add("blurb__save");
+        }
+    };
+
+    const handleEdit = () => {
+        if (editSelected) {
+            return (
+                <div className="blurb__container">
+                    <TextField
+                        type="text"
+                        value={heading}
+                        inputProps={{ className: classes.heading }}
+                    />
+                    <TextareaAutosize
+                        className={classes.content}
+                        id="text-area"
+                        type="text"
+                        multiline
+                        rowsMin={3}
+                        variant="outlined"
+                        value={content}
+                        onChange={(e) => contentChange(e)}
+                    />
+                    <div className="blurb__btns">
+                        <button
+                            onClick={() => {
+                                setEditSelected(!editSelected);
+                                setContent(props.ele.content.content);
+                            }}
+                            className="blurb__cancel"
+                        >
+                            Cancel
+                        </button>
+                        <button id="save-btn" className="blurb__nochange">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="blurb__container">
+                    <p className="blurb__heading">
+                        {props.ele.content.heading}
+                    </p>
+                    <p className="blurb__content">
+                        {props.ele.content.content}
+                    </p>
+                </div>
+            );
         }
     };
 
@@ -31,7 +117,13 @@ const DndBlurb = (props) => {
             <div className="blurb__tag">
                 <p className="blurb__label">Blurb</p>
                 <div>
-                    <button className="sandbox__btn-wrap">
+                    <button
+                        className="sandbox__btn-wrap"
+                        onClick={() => {
+                            setEditSelected(!editSelected);
+                            setContent(props.ele.content.content);
+                        }}
+                    >
                         <img
                             className="sandbox__icon"
                             src={Edit}
@@ -50,11 +142,13 @@ const DndBlurb = (props) => {
                     </button>
                 </div>
             </div>
-            <div className="blurb__container">
-                <p className="blurb__heading">{props.ele.content.heading}</p>
-                <p className="blurb__content">{props.ele.content.content}</p>
-            </div>
-            <DeleteDiv deleteSelected={deleteSelected} ele={props.ele} setDeleteSelected={setDeleteSelected} setContentLoading={props.setContentLoading}/>
+            {handleEdit()}
+            <DeleteDiv
+                deleteSelected={deleteSelected}
+                ele={props.ele}
+                setDeleteSelected={setDeleteSelected}
+                setContentLoading={props.setContentLoading}
+            />
         </div>
     );
 };
