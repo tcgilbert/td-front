307ef68ddc49d1041cab968159c0a55b67e4702a
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-19 12:43:45 -0500

    [feedLink] basic styling for feedlink

--- src/components/feed/FeedLink.js
+++ src/components/feed/FeedLink.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const FeedLink = (props) => {
+
+    const { content } = props.ele;
+    const url = content.url
+    useEffect(() => {
+
+        const link = document.getElementById("link")
+        link.addEventListener("click", () => {
+            window.open(url, "_blank")
+        })
+
+    }, [])
+
     return (
-        <div className="feedlink">
-            {props.ele.type}
+        <div id="link" className="feedlink">
+            <span className="feedlink__title">{content.title}</span>
         </div>
     )
 }
